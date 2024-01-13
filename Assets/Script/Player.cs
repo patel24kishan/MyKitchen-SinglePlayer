4397556b3dce1e733603ed16a9d7670569098c11
ghosts24 <35022263+patel24kishan@users.noreply.github.com>
2024-01-13 10:59:20 -0400

    Fix Player Movement
    Add logic to HandleMovement to fix Player looking at counter

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -110,7 +110,7 @@ public class Player : MonoBehaviour,IKitchenObjectParent
         {
             //Attempt only x
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x!=0 && !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -121,7 +121,7 @@ public class Player : MonoBehaviour,IKitchenObjectParent
             {
                 //Attempt only  Z
                 Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius, moveDirZ, moveDistance);
+                canMove =  moveDir.z!=0 && !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius, moveDirZ, moveDistance);
                 
                 if (canMove)
                 {
